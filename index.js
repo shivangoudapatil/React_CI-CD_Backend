1ace8ebd34933cb312f3fe306015658b2c893583
Shivangouda Patil <91386977+shivangoudapatil@users.noreply.github.com>
2023-04-23 08:08:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const port = 3333;
 app.use(cors());
 app.use('/api', apiRoutes);
 
+app.get('/',(req, res) => {res.json({mesg:"hello from backend"})})
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
